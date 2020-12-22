b97e6fbc17b4854cffb91412e9e88fcd0b9249d4
LaimQQl <laingt20012@gmail.com>
2020-12-22 14:50:51 +0300

    Копирование и объединение объектов

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
-alert("First alert");
+//Работы с объектами
+'use strict'
+
+//Клонирование и объединение объектов
+
+//Объединение
+let block = {
+    type: "square",
+    size: 20,
+};
+
+let width = {
+    width: 30,
+};
+
+let height = {
+    height: 30,
+};
+
+Object.assign(block, width, height);
+
+console.log(block);
+//
+
+//Клонирование
+
+let user = {
+    name: "Denis",
+    age: 19,
+}
+
+let cloneUser = Object.assign({}, user);
+
+console.log(cloneUser);
+//
+
+//Вложенное копирование
+let figure = {
+    name: "triangle",
+    size: {
+        a: 10,
+        b: 8,
+        c: 12,
+    }
+};
+
+let cloneBlock = Object.assign({}, figure);
+
+figure.size.a++;
+
+console.log(cloneBlock.size.a);
+//
