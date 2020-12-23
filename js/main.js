eb2fe8fd54cb5707d20af1747281cf8f6dc53813
LaimQQl <laingt20012@gmail.com>
2020-12-23 10:21:26 +0300

    Методы

--- js/main.js
+++ js/main.js
@@ -1,52 +1,19 @@
-//Работы с объектами
 'use strict'
-
-//Клонирование и объединение объектов
-
-//Объединение
-let block = {
-    type: "square",
-    size: 20,
-};
-
-let width = {
-    width: 30,
-};
-
-let height = {
-    height: 30,
-};
-
-Object.assign(block, width, height);
-
-console.log(block);
-//
-
-//Клонирование
-
+//Пример метода SayHi()
 let user = {
     name: "Denis",
     age: 19,
-}
-
-let cloneUser = Object.assign({}, user);
-
-console.log(cloneUser);
-//
-
-//Вложенное копирование
-let figure = {
-    name: "triangle",
-    size: {
-        a: 10,
-        b: 8,
-        c: 12,
+    sayHi() {//При создании метода, можно не объявлять function
+        alert(`${this.name}: Hello!`);
     }
 };
 
-let cloneBlock = Object.assign({}, figure);
+user.sayHi();
+//
 
-figure.size.a++;
-
-console.log(cloneBlock.size.a);
+//Конструкторы, создание объектов через "new"
+function Person(fio) {
+    this.fio = fio;
+}
+console.log()
 //
